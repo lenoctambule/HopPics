f0db78917340efa704c0ac0d9e9693e3cb2d707a
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-12-15 13:29:28 +0100

    fix: missing learning rate

--- ClassicHopfieldNet.py
+++ ClassicHopfieldNet.py
@@ -3,7 +3,6 @@ import tqdm
 import random as rd
 
 class HopfieldNet:
-
 	def __init__(self, data_len, learning_rate):
 		self.data_len = data_len
 		self.learning_rate = learning_rate
@@ -12,7 +11,7 @@ class HopfieldNet:
 
 	def train(self,v):
 		self.N += 1
-		self.W += np.outer(v, v)
+		self.W += np.outer(v, v) * self.learning_rate
 
 	def compute_step(self, v_n):
 		v_n += np.einsum('ij,j->i', self.W, v_n)
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from HopPics import *
 import sys
 
 if __name__ == "__main__" :
-	if len(sys.argv) != 3 :
-		print("Usage : py test.py <image_path> <nsteps>")
+	if len(sys.argv) != 4 :
+		print("Usage : py test.py <image_path> <nsteps> <noise_amount>")
 		exit()
-	hp = HopPics(sys.argv[1], 50)
-	hp.reconstruct_from_noise(noise_amount=1_000, n_steps=int(sys.argv[2]))
+	hp = HopPics(sys.argv[1], 0.1)
+	hp.reconstruct_from_noise(noise_amount=int(sys.argv[3]), n_steps=int(sys.argv[2]))
